adf2761fa129fdc57029d3f71a6b6c403f6e23a9
Rob Braden <rob.braden@gmail.com>
2023-02-15 15:58:10 -0800

    (#1) Add checksum verification to download script
    Fixes #1

--- kafka/setup_builds.sh
+++ kafka/setup_builds.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 #
 
-#TODO verify the download and such
-curl -s -O -L https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
-curl -s -O -L https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz.sha512
-curl -s -O -L https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz.asc
+kafka_version='3.4.0'
+scala_version='2.13'
+target_filename="kafka_${scala_version}-${kafka_version}.tgz"
 
-tar zxf kafka*tgz
+download_location="https://downloads.apache.org/kafka"
+
+curl --silent --remote-name --location "${download_location}"/"${kafka_version}"/"${target_filename}"
+curl --silent --remote-name --location "${download_location}"/"${kafka_version}"/"${target_filename}.sha512"
+curl --silent --remote-name --location "${download_location}"/"${kafka_version}"/"${target_filename}.asc"
+
+# TODO Better than nothing but we should also check the signature
+if gpg --print-md SHA512 "${target_filename}" | diff - "${target_filename}.sha512" ; then
+  tar zxf "${target_filename}"
+else
+  echo "ERROR: Checksum mismatch"
+  exit 1
+fi
